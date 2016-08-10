f44fd9ef37f91c9e9c4156f16c418b96a0278b8c
Yushe <Yushe@users.noreply.github.com>
2016-08-10 18:19:59 +0100

    Fix gas somewhat

--- gamemodes/zombiesurvival/entities/entities/zombiegasses/init.lua
+++ gamemodes/zombiesurvival/entities/entities/zombiegasses/init.lua
@@ -4,11 +4,9 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 function ENT:Initialize()
-	self.Heal = self.Heal or 25
+	self.Heal = self.Heal or 20
 	self:DrawShadow(false)
 	self:Fire("attack", "", 1.5)
-
-	if self:GetRadius() == 0 then self:SetRadius(400) end
 end
 
 function ENT:KeyValue(key, value)
@@ -18,6 +16,10 @@ function ENT:KeyValue(key, value)
 	elseif key == "heal" then
 		self.Heal = tonumber(value) or self.Heal
 	end
+end
+
+function ENT:UpdateTransmitState()
+	return TRANSMIT_PVS
 end
 
 local function TrueVisible(posa, posb)
@@ -30,26 +32,24 @@ local function TrueVisible(posa, posb)
 end
 
 function ENT:AcceptInput(name, activator, caller, arg)
-	if name ~= "attack" then return end
-	self:Fire("attack", "", 1.5)
+	if name == "attack" then
+		self:Fire("attack", "", 1.5)
 
-	if GAMEMODE:GetWave() <= 0 or GAMEMODE.ZombieEscape then return end
-
-	local vPos = self:GetPos()
-	for _, ent in pairs(ents.FindInSphere(vPos, self:GetRadius())) do
-		if ent:IsPlayer() and ent:Alive() and WorldVisible(vPos, ent:NearestPoint(vPos)) then
-			if ent:Team() == TEAM_UNDEAD then
-				--[[if ent:Health() < ent:GetMaxHealth() and not ent:GetZombieClassTable().Boss then
-					ent:SetHealth(math.min(ent:GetMaxZombieHealth(), ent:Health() + self.Heal))
-					ent.m_LastGasHeal = CurTime()
-				end]]
-				ent:GiveStatus("zombiespawnbuff", 3)
-			elseif 1 < ent:Health() then
-				--ent:PoisonDamage(math.min(10, ent:Health() - 1), self, self)
-				ent:PoisonDamage(math.min(5, ent:Health() - 1), self, self)
+		if 0 < GAMEMODE:GetWave() then
+			local vPos = self:GetPos()
+			for _, ent in pairs(ents.FindInSphere(vPos, self:GetRadius())) do
+				if ent:IsPlayer() and ent:Alive() and TrueVisible(vPos, ent:EyePos()) then
+					if ent:Team() == TEAM_UNDEAD then
+						if GAMEMODE:GetFighting() and CurTime() < GAMEMODE:GetWaveEnd() then
+							ent:SetHealth(math.min(ent.ClassTable.Health, ent:Health() + self.Heal))
+						end
+					elseif 5 < ent:Health() then
+						DoPoisoned(ent, self, "dumb")
+					end
+				end
 			end
 		end
-	end
 
-	return true
+		return true
+	end
 end
